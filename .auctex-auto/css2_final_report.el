537ced65437805e95ef0ac15f577682813a735da
Euan Mendoza <bebopbamf@effectfree.dev>
2024-05-17 08:43:03 +1000

    updated report with information on combinatorial feature selection problems

--- .auctex-auto/css2_final_report.el
+++ .auctex-auto/css2_final_report.el
@@ -3,13 +3,18 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("IEEEtran" "conference")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("biblatex" "style=ieee")))
    (TeX-run-style-hooks
     "latex2e"
     "preamble"
     "IEEEtran"
     "IEEEtran10"
-    "cite")
-   (LaTeX-add-bibitems
-    "IEEEhowto:kopka"))
+    "biblatex")
+   (LaTeX-add-labels
+    "def:k-feature-selection"
+    "table:k-feat-select-example")
+   (LaTeX-add-bibliographies
+    "instagram-fake-detection"))
  :latex)
 
