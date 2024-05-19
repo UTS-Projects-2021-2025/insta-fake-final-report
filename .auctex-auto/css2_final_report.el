5d7de4192c5cbd89ec9b2618f353851bc24d5a29
Euan Mendoza <bebopbamf@effectfree.dev>
2024-05-19 20:55:41 +1000

    added results tables and diagrams

--- .auctex-auto/css2_final_report.el
+++ .auctex-auto/css2_final_report.el
@@ -5,10 +5,10 @@
                      '(("IEEEtran" "conference")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("biblatex" "style=ieee")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "preamble"
@@ -25,7 +25,13 @@
     "table:memetic-brute-force"
     "sec:solutions"
     "fig:correlation-heatmap"
-    "fig:class-distribution")
+    "fig:class-distribution"
+    "table:xgboost-results"
+    "fig:xg-roc-curve"
+    "fig:xg-roc-curve-reduced"
+    "table:svm-results"
+    "fig:svm-roc-curve"
+    "fig:svm-roc-curve-reduced")
    (LaTeX-add-bibliographies
     "instagram-fake-detection"))
  :latex)
